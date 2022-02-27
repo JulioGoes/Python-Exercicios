03d07317d868d52187debf1633aecc48392aaba1
JulioGoes <goes.cesar157@gmail.com>
2022-02-27 12:06:40 -0300

    update

--- src/1. EstruturaSequencial/exercicio02.py
+++ src/1. EstruturaSequencial/exercicio02.py
@@ -1,4 +1,4 @@
-# Faça um Programa que peça um número e então mostre a mensagem 
+# Faça um Programa que peça um número e então mostre a mensagem
 # O número informado foi [número].
 num = input('Digite um número: ')
 print('O número informado foi {}'.format(num))
